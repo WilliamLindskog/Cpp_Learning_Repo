c2f5c9bac3e0c21d9553922a3df7dc70390fa0a9
William Lindskog <w.lindskog@eu.denso.com>
2021-08-24 17:12:19 +0200

    New lesson math operators with Tim

---
+++ C++_with_Tim/mathOperations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+	float x = 8;
+	x *= 2;
+	cout << x; 
+}
+
+//int main()
+//{
+	//int x = 8;
+	//float y = 9;
+	//float sum = x / y; 
+	//cout << sum; 
+	// Float has higher level of precision
+
+
+//}
