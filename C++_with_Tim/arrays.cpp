5c8fdf8f1e7d6178e4b62df6d5251875cb44919a
William Lindskog <w.lindskog@eu.denso.com>
2021-08-30 16:53:52 +0200

    Learned about arrays with Tim

---
+++ C++_with_Tim/arrays.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	int x = 5;
+	int arr[x] = {1, 2, 3, 4, 5};
+	cout << arr << "\n"; // location of array in computer's memory
+	cout << arr[0];
+}
