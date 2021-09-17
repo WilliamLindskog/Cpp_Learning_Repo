271ece99461814e4152c247ce23be352b91550e8
William Lindskog <w.lindskog@eu.denso.com>
2021-09-17 15:37:07 +0200

    First mini project

---
+++ HelloWorld/hello.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* The simplest C Program */
+
+int main(int argc, char **argv)
+{
+printf("Hello World\n");
+
+return 0;
+}	
---
+++ mini_projects/login_registration.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std; 
+
+bool IsLoggedIn(){
+    string username, password, un, pw;
+
+    cout << "Enter Username: "; cin >> username;
+    cout << "Enter Password: "; cin >> password; 
+
+    ifstream read("data\\" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username  && pw == password){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main(){
+    int choice; 
+    cout << "1: Register\n2: Login\nYour Choice: "; cin >> choice;
+
+    if (choice == 1){
+        string username, password; 
+
+        cout << "Select a Username: "; cin >> username; 
+        cout << "Select a Password: "; cin >> password; 
+
+        ofstream file; 
+        file.open("data\\" + username + ".txt");
+        file << username << endl << password;
+        file.close();
+
+        main();
+    } else if(choice == 2){
+        bool status = IsLoggedIn();
+
+        if (!status){
+            cout << "False Credentials" << endl;
+            system("PAUSE");
+            return 0; 
+        } else {
+            cout << "Successfully Logged In" << endl;
+            system("PAUSE");
+            return 1;
+        }
+    } 
+}
